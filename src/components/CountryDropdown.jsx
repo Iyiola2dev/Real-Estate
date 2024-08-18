c4b494ab6b20e2371d043bba60c4a1f69d9051ff
Lexicon <iyiolad21@gmail.com>
2024-08-18 01:31:51 +0100

    I added comment for code clarification

--- src/components/CountryDropdown.jsx
+++ src/components/CountryDropdown.jsx
@@ -37,8 +37,9 @@ const CountryDropdown = () => {
           )}
       </Menu.Button>
       <Menu.Items className='dropdown-menu'>
-
+    
         {countries.map((country, index)=>{
+          {/* this is to map through the countries data which was change to country and it manipulate the dropdown of all the countries array with the use of setCoutries(uniqueCountries) or countries.map iterates over the countries array (which includes "Location (any)" and unique countries).*/}
           return(
             <Menu.Item onClick={()=>setCountry(country)} as="li" key={index} className="cursor-pointer hover:to-violet-700 transition"> {country}</Menu.Item>
           )
