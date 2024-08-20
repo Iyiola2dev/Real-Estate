1871a397969fab84ef1cac66951b48772bf75693
Lexicon <iyiolad21@gmail.com>
2024-08-20 23:02:21 +0100

    correction of word 'rent'

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -8,7 +8,7 @@ const Banner = () => {
       <div className="flex lg:flex-row flex-col ">
         <div className="lg:ml-8 xl:ml-[135px] flex flex-col items-center lg:items-start text-center lg:text-left justify-center flex-1 px-4 lg:px-0">
           <h1 className="text-4xl lg:text-[58px] font-semibold leading-none mb-6  space-x-2">
-            <span className="text-violet-700">Rest</span> Your Dream House With
+            <span className="text-violet-700">Rent</span> Your Dream House With
             Us.
           </h1>
           <p className="max-w-[480px] mb-8">
