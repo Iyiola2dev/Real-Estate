82e3962a73d037857ae838eae19a5fe47870a5e3
Lexicon <iyiolad21@gmail.com>
2024-08-20 18:19:54 +0100

    I inputed some details into property details

--- src/pages/PropertyDetails.jsx
+++ src/pages/PropertyDetails.jsx
@@ -23,16 +23,57 @@ const PropertyDetails = () => {
   return (
     <section>
       <div className="container mx-auto min-h[800px] mb-14">
-        <div>
+        <div className="flex flex-col lg:flex-row lg:items-center lg:justify-between">
           <div>
-            <h2>{house.name}</h2>
-            <h3>{house.address}</h3>
+            <h2 className="text-2xl font-semibold">{house.name}</h2>
+            <h3 className="text-lg mb-4">{house.address}</h3>
           </div>
-          <div>
-            <div>{house.type}</div>
-            <div>{house.country}</div>
+          <div className="mb-4 lg:mb:0 flex gap-x-2 text-sm">
+            <div className="bg-green-500 text-white rounded-full px-3">
+              {house.type}
+            </div>
+            <div className="bg-violet-500 text-white rounded-full px-3">
+              {house.country}
+            </div>
           </div>
-          <div>{house.price}</div>
+          <div className="text-3xl font-semibold text-violet-600">
+            ₦ {house.price}
+          </div>
+        </div>
+        <div className="flex flex-col items-start gap-8 lg:flex-row ">
+          <div className="max-w-[768px]">
+            <div className="mb-8">
+              <img src={house.imageLg} alt="" />
+            </div>
+            <div className="flex gap-x-6 text-violet-700 mb-6">
+              <div className="flex justify-center items-center  gap-x-2">
+                <BiBed className="text-2xl" />
+                <div>{house.bedrooms}</div>
+              </div>
+              <div className="flex justify-center items-center gap-x-2">
+                <BiBath className="text-2xl" />
+                <div>{house.bathrooms}</div>
+              </div>
+              <div className="flex justify-center items-center gap-x-2">
+                <BiArea className="text-2xl" />
+                <div>{house.surface}</div>
+              </div>
+            </div>
+            <div>{house.description}</div>
+          </div>
+          <div className="flex-1 bg-white w-full mb-8 border border-gray-300 rounded-lg px-6 py-8">
+            <div className="flex items-center gap-x-4 mb-8">
+              <div className="w-20 h-20 p-1 border">
+                <img src={house.agent.image} alt="" />
+              </div>
+              <div>
+                <div>{house.agent.name}</div>
+                <Link to="" className="text-violet-700 text-sm">
+                  View Listings
+                </Link>
+              </div>
+            </div>
+          </div>
         </div>
       </div>
     </section>
